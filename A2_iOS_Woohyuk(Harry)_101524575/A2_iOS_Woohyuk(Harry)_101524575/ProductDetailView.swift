4478280fc072e6de1ecc366a60a1663304e961b7
Woohyuk Song <harrywsong@naver.com>
2026-04-09 06:16:15 -0400

    temporarily replaced contentview with placeholder for previews

---
+++ A2_iOS_Woohyuk(Harry)_101524575/A2_iOS_Woohyuk(Harry)_101524575/ProductDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  ProductDetailView.swift
+//  A2_iOS_Woohyuk(Harry)_101524575
+//
+//  Created by Woohyuk Song on 2026-04-09.
+//
+
+import SwiftUI
+
+struct ProductDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProductDetailView()
+}
