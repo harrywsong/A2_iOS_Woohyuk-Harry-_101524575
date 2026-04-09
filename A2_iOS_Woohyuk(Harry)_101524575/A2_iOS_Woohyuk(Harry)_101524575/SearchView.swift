03ced89bcce414f90608cf61eab01239b5d3b1dd
Woohyuk Song <harrywsong@naver.com>
2026-04-09 06:53:26 -0400

    Style product name and description text in SearchView

--- A2_iOS_Woohyuk(Harry)_101524575/A2_iOS_Woohyuk(Harry)_101524575/SearchView.swift
+++ A2_iOS_Woohyuk(Harry)_101524575/A2_iOS_Woohyuk(Harry)_101524575/SearchView.swift
@@ -30,7 +30,9 @@ struct SearchView: View {
             List(filtered, id: \.id) { product in
                 VStack(alignment: .leading, spacing: 4) {
                     Text(product.name ?? "")
+                        .font(.headline)
                     Text(product.productDescription ?? "")
+                        .font(.subheadline)
                 }
                 .padding(4)
             }
